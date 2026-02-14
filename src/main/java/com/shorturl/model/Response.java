e948d57feb5622b08dea0d889c7ac58d092bdc8b
davinash97 <avinashd2904@gmail.com>
2026-02-14 22:57:46 +0530

    [response] clean up

--- src/main/java/com/shorturl/model/Response.java
+++ src/main/java/com/shorturl/model/Response.java
@@ -5,28 +5,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 public class Response {
 
-	private final int http_code;
+	private final int code;
 	private final String status;
-	private String body;
 	private String short_url;
 	private String long_url;
 
 	
-	public Response(int http_code, String status, String body) {
-		this.http_code = http_code;
+	public Response(int code, String status) {
+		this.code = code;
 		this.status = status;
-		this.body = body;
 	}
 
-	public Response(int http_code, String status, String short_url, String long_url) {
-		this.http_code = http_code;
+	public Response(int code, String status, String short_url, String long_url) {
+		this.code = code;
 		this.status = status.toLowerCase();
 		this.short_url = short_url;
 		this.long_url = long_url;
 	}
 
-	public int getHttpCode() {
-		return http_code;
+	public int getCode() {
+		return code;
 	}
 
 	public String getStatus() {
@@ -39,10 +37,6 @@ public class Response {
 
 	public String getLongUrl() {
 		return long_url;
-	}
-
-	public String getBody() {
-		return body;
 	}
 
 }
