cc9246bb9b06e3fb8962fd66d0fef5934ca5a10b
davinash97 <avinashd2904@gmail.com>
2026-02-14 14:03:01 +0530

    [encode] improvise key generation - add .env to .gitignore

--- src/main/java/com/shorturl/core/UrlCore.java
+++ src/main/java/com/shorturl/core/UrlCore.java
@@ -1,19 +1,22 @@
 package com.shorturl.core;
 
 public class UrlCore {
-	
-	private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ";
+
+	private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static final int BASE = 62;
 
 	public static String encode(long value) {
-		if(value < 0) throw new IllegalArgumentException("value should be greater than 0");
+		if (value < 0)
+			throw new IllegalArgumentException("value should be greater than 0");
 
-		if(value == 0) ALPHABET.charAt(0);
+		if (value == 0)
+			return String.valueOf(ALPHABET.charAt(0));
+		
 		StringBuilder str = new StringBuilder();
 
 		long temp = value;
-		while(temp > 0) {
-			int remainder = (int) temp % BASE;
+		while (temp > 0) {
+			int remainder = (int) (temp % BASE);
 			temp /= BASE;
 			str.append(ALPHABET.charAt(remainder));
 		}
--- src/main/java/com/shorturl/service/UrlService.java
+++ src/main/java/com/shorturl/service/UrlService.java
@@ -1,8 +1,8 @@
 package com.shorturl.service;
 
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
@@ -11,20 +11,22 @@ import com.shorturl.core.UrlCore;
 @Service
 public class UrlService {
 
-	private static Random random = new Random(6);
+	private static final SecureRandom random = new SecureRandom();
 
 	private static Map<String, String> map = new HashMap<>();
 
 	public static String longToShort(String url) {
-		long id = random.nextLong(10_000_000, 1_00_000_000);
-		System.out.println("Generated random number is " + id);
-		String key = UrlCore.encode(id);
-		if(!map.containsKey(key)) {
-			map.put(key, url);
-		}
+		String key;
+		do {
+			long id = random.nextLong(10_000_000L, 100_000_000L);
+			key = UrlCore.encode(id);
+		} while (map.containsKey(key));
+
+		map.put(key, url);
 		return key;
 	}
 
+	// Implement (if key not exist)
 	public static String shortToLong(String key) {
 		return map.get(key);
 	}
