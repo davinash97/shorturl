e84f9aaf21c80181c346003bb65fb7da9c5037ed
davinash97 <avinashd2904@gmail.com>
2026-08-09 22:15:24 +0530

    remove config interface

--- src/main/java/com/shorturl/common/config/Config.java
+++ src/main/java/com/shorturl/common/config/Config.java
@@ -1,11 +1,39 @@
 package com.shorturl.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-public interface Config {
-	public int getPort();
-	public String getDbUrl();
-	public String getDbUsername();
-	public String getDbPassword();
+@PropertySource(value = "file://${user.dir}/.env")
+public class Config {
+
+	@Value("${server.port}")
+	private int PORT;
+
+	@Value("${spring.datasource.url}")
+	private String DB_URL;
+
+	@Value("${spring.datasource.username}")
+	private String DB_USERNAME;
+
+	@Value("${spring.datasource.password}")
+	private String DB_PASSWORD;
+
+	public int getPort() {
+		return PORT;
+	}
+
+	public String getDbUrl() {
+		return DB_URL;
+	}
+
+	public String getDbUsername() {
+		return DB_USERNAME;
+	}
+
+	public String getDbPassword() {
+		return DB_PASSWORD;
+	}
+
 }
