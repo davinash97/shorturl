4052968d651bd9043f73496392331e9356f9b3a3
davinash97 <avinashd2904@gmail.com>
2026-08-09 22:15:03 +0530

    Add Spring Security

---
+++ src/main/java/com/shorturl/security/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.shorturl.security.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable())
+				.cors(cors -> {
+				})
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.GET, "/").permitAll()
+						.requestMatchers("/api/v1/auth/**").permitAll()
+						.anyRequest().authenticated())
+				.httpBasic(httpBasic -> {
+				});
+
+		return http.build();
+	}
+}
+
+	// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+	// Exception {
+	// http.authorizeHttpRequests(
+	// authorize -> authorize
+	// .requestMatchers(HttpMethod.GET, "/").permitAll()
+	// .requestMatchers("/api/v1/**").permitAll()
+	// .requestMatchers(
+	// // "/api/v1",
+	// "/api/v1/user/**",
+	// "/api/v1/url/**")
+	// .authenticated()
+	// .anyRequest().authenticated()
+	// )
+	// .httpBasic(httpBasic -> {
+	// });
+
+	// return http.build();
+	// }
